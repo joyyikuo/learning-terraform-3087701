ee04c7e2711660d985fe18faf117e94bfba845ce
Yi Kuo <yi.kuo@ocado.com>
2024-07-26 11:22:02 +0100

    dev: attempt bigquery IAM user add

--- main.tf
+++ main.tf
@@ -1,17 +1,5 @@
-resource "google_compute_instance" "vm_instance" {
-  name         = "terraform-instance"
-  machine_type = "e2-micro"
-
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-    }
-  }
-
-  network_interface {
-    # A default network is created for all GCP projects
-    network = "default"
-    access_config {
-    }
-  }
+resource "google_project_iam_member" "bigquery_user" {
+  member  = "user:kuoj12@gmail.com"
+  project = var.project_id
+  role    = "roles/bigquery.admin"
 }
