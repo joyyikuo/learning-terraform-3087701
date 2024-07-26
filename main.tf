3156e498b8c5a249a2a10b0677db5bb234f1427a
Yi Kuo <yi.kuo@ocado.com>
2024-07-26 11:45:17 +0100

    dev: test IAM removal

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
-resource "google_project_iam_member" "bigquery_user" {
-  member  = "user:kuoj12@gmail.com"
-  project = var.project_id
-  role    = "roles/bigquery.admin"
-}
+#resource "google_project_iam_member" "bigquery_user" {
+#  member  = "user:kuoj12@gmail.com"
+#  project = var.project_id
+#  role    = "roles/bigquery.admin"
+#}
